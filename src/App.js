6878310254eded0c79ac0fac5f5261f155de2dea
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-22 16:55:56 +0100

    passed in users as props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/User';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
-        <h1> Hi</h1>
+        <Users />
       </div>
     );
   }
